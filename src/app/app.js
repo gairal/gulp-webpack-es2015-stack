c617870267f472db03b0b94d0516d416cbcaf397
frank.gairal <gairal@ekino.com>
2018-07-10 16:47:05 -0400

    fix(init): using chooles for main init

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,4 @@
+import Chooles from 'chooles';
 import init from './gairal';
 
-/* eslint-disable-next-line no-confusing-arrow */
-((g, a) =>
-  g.readyState === 'loading' ? g.addEventListener('DOMContentLoaded', a) : a())(
-  document,
-  init,
-);
+Chooles.ready(init);
