bdc4a60b7a8ff639754c0f928d638149975cb022
frank.gairal <gairal@ekino.com>
2018-09-10 13:52:31 -0700

    fix(deps): dependencies upgrade

--- src/app/app.js
+++ src/app/app.js
@@ -1,19 +1,20 @@
 import 'es6-promise/auto';
 import 'whatwg-fetch';
-import Loggout from 'loggout';
+// import Loggout from 'loggout';
 
-import GLOBALS from './core/GLOBALS';
+// import GLOBALS from './core/GLOBALS';
 import Calculator from './components/Calculator';
 
 import '../scss/app.scss';
 
 const init = () => {
+  const GLOBALS = {};
   // COMPONENT CREATION
   [
-    {
-      name: 'logger',
-      class: Loggout,
-    },
+    // {
+    //   name: 'logger',
+    //   class: Loggout,
+    // },
     {
       name: 'calculator',
       class: Calculator,
